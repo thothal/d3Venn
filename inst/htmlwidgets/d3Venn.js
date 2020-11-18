8a92d5f8a19590878d88f9162d87f5916a5600f7
Thorn Thaler <t.thaler@crifbuergel.de>
2020-11-18 14:40:05 +0100

    Change venn.js to maintained version

---
+++ inst/htmlwidgets/d3Venn.js
@@ -0,0 +1,27 @@
+HTMLWidgets.widget({
+
+  name: "d3Venn",
+
+  type: "output",
+
+  factory: function(el, width, height) {
+
+    // create our d3 venn object
+    var chart = venn.VennDiagram();
+
+    return {
+      renderValue: function(x) {
+         console.log(x);
+         d3.select(el).datum(x.data).call(chart);
+      },
+
+      resize: function(width, height) {
+      },
+
+      // Make the sigma object available as a property on the widget
+      // instance we're returning from factory(). (maybe not really needed
+      // as all the modifications go through the d3 framework
+      chart: chart
+    };
+  }
+});
