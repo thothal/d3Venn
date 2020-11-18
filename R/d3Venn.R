7c90729ceb53c0f1bc5f0df5f83599d49fd7e0a7
Thorn Thaler <t.thaler@crifbuergel.de>
2020-11-18 14:39:23 +0100

    Fix data format for passing to JS

--- R/d3Venn.R
+++ R/d3Venn.R
@@ -3,7 +3,7 @@
 #' Draw a Venn diagram given a data frame with the sizes and names of the sets.
 #'
 #' To pass the size (and or label) of the intersections, the corresponding `sets` element
-#' must contain a list. To create `list` columns you can use [base:I()]. See Examples.
+#' must contain a list. To create `list` columns you can use [base::I()]. See Examples.
 #'
 #' @param sets data.frame, each row correspond to one part in the Venn diagram. Must
 #'        contain columns `sets` and `size` and may contain a column `label`.
@@ -12,11 +12,11 @@
 #' @param height integer, height of the htmlwidget.
 #'
 #' @section JavaScript Libraries:
-#' * <https://github.com/benfred/venn.js/>
+#' * <https://github.com/upsetjs/venn.js>
 #' * <https://d3js.org/>
 #'
 #' @return an `htmlwidget`
-#' @seealso [htmlwidgets::createWidget]
+#' @seealso [htmlwidgets::createWidget()]
 #' @export
 #'
 #' @examples
@@ -27,11 +27,11 @@
 #' d3Venn(data.frame(sets = c("A", "B"), size = c(10, 20), label = c("Set 1", "Set 2")))
 #'
 #' # Venn diagram with 2 overlapping sets
-#' d3Venn(data.frame(sets = I(list("A", "B", list("A", "B")))
+#' d3Venn(data.frame(sets = I(list("A", "B", list("A", "B"))),
 #'                   size = c(10, 10, 2)))
 #'
 #' # Venn diagram where B is a subset of A
-#' d3Venn(data.frame(sets = I(list("A", "B", list("A", "B")))
+#' d3Venn(data.frame(sets = I(list("A", "B", list("A", "B"))),
 #'                   size = c(20, 10, 10)))
 
 d3Venn <- function(sets,
@@ -41,7 +41,11 @@ d3Venn <- function(sets,
    ## `sets` will most likely include list columns with list elements, so use
    ## data.frame(sets = I(list(1, 2, list(1, 2))))
 
-   data <- do.call(function(...) Map(list,...), sets)
+   data <- do.call(function(...) Map(function(...) {
+      res <- list(...)
+      res$sets <- as.list(res$sets)
+      res
+   }, ...), sets)
    htmlwidgets::createWidget(
       name = "d3Venn",
       data,
